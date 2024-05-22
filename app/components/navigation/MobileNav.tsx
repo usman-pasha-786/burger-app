94ee75e9219cf1ae0018a52af5479b3eb15b8ab9
usman-pasha-786 <147035343+usman-pasha-786@users.noreply.github.com>
2024-05-23 00:00:07 +0500

    complete and uploaded nav bar

--- app/components/navigation/MobileNav.tsx
+++ app/components/navigation/MobileNav.tsx
@@ -1,8 +1,43 @@
+import Link from 'next/link'
 import React from 'react'
+import { ImCross } from 'react-icons/im'
 
-const MobileNav = () => {
+interface iprops{
+  showNav:boolean,
+  closeNav:()=>void
+}
+
+const MobileNav = ({showNav,closeNav}:iprops) => {
+
+const navStyle = showNav? 'translate-x-0' : 'translate-x-[-100%]'
+
   return (
-    <div>MobileNav</div>
+    <div className={`fixed ${navStyle} right-0 transition-all duration-500 left-0 top-0 bottom-0 h-[100vh] bg-[#000000e0] z-[1002]`}>
+      <ImCross onClick={closeNav} className='absolute top-[2rem] right-[2rem] w-[2rem] h-[2rem] text-white'/>
+
+      {/* Nav Div */}
+      <div className={`bg-emerald-700 ${navStyle} transition-all duration-500 delay-200 flex flex-col items-center justify-center w-[70%] h-[100%]`}>
+         
+        {/* Nav Link */}
+        <ul className='space-y-10'>
+          <li className='text-[35px] font-medium hover:text-yellow-400 text-white'>
+            <Link href='/'>Home</Link>
+          </li>
+          <li className='text-[35px] font-medium hover:text-yellow-400 text-white'>
+            <Link href='/'>Shop</Link>
+          </li>
+          <li className='text-[35px] font-medium hover:text-yellow-400 text-white'>
+            <Link href='/'>Menu</Link>
+          </li>
+          <li className='text-[35px] font-medium hover:text-yellow-400 text-white'>
+            <Link href='/'>Blog</Link>
+          </li>
+          <li className='text-[35px] font-medium hover:text-yellow-400 text-white'>
+            <Link href='/'>Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
--- app/components/navigation/Nav.tsx
+++ app/components/navigation/Nav.tsx
@@ -1,8 +1,61 @@
+import Link from 'next/link'
 import React from 'react'
+import { BiCycling, BiShoppingBag } from 'react-icons/bi'
+import { FaBurger } from 'react-icons/fa6'
+import { HiBars3BottomRight } from 'react-icons/hi2'
 
-const Nav = () => {
+interface iprops{
+  openNav:()=>void
+}
+
+const Nav = ({openNav}:iprops) => {
   return (
-    <div>Nav</div>
+    <div className='h-[12vh] bg-blue-400'>
+      <div className='sm:w-[90%] w-[95%] mx-auto flex h-[100%] items-center justify-between'>
+        
+        {/* logo */}
+        <div className='flex items-center space-x-2'>
+          <FaBurger className='w-[1.2rem] h-[1.2rem] sm:w-[1.4rem] sm:h-[1.4rem] text-orange-500'/>
+          <h1 className='text-[20px] sm:text-[30px] font-semibold'>BurgerBite</h1>
+        </div>
+
+
+        {/* Navbar links */}
+        <ul className='hidden lg:flex items-center space-x-10'>
+          <li className='text-[20px] font-medium hover:text-red-600'>
+            <Link href='/'>Home</Link>
+          </li>
+          <li className='text-[20px] font-medium hover:text-red-600'>
+            <Link href='/'>Shop</Link>
+          </li>
+          <li className='text-[20px] font-medium hover:text-red-600'>
+            <Link href='/'>Menu</Link>
+          </li>
+          <li className='text-[20px] font-medium hover:text-red-600'>
+            <Link href='/'>Blog</Link>
+          </li>
+          <li className='text-[20px] font-medium hover:text-red-600'>
+            <Link href='/'>Contact</Link>
+          </li>
+        </ul>
+
+        {/* buttons */}
+        <div className='flex items-center space-x-4'>
+          <button className='bg-blue-950 text-white px-6 py-2 sm:px-8 sm:py-3 text-[14px] sm:text-[16px] transition-all duration-200 hover:bg-red-600 flex items-center rounded-md space-x-2'>
+            <span>
+              <BiCycling className='w-[1.3rem] h-[1.3rem] sm:w-[1.7rem] sm:h-[1.7rem]'/>
+            </span>
+            <span className='font-bold'>Order Now</span>
+          </button >
+          <button className='sm:px-6 sm:py-3 px-4 py-2 hover:bg-gray-700 transition-all duration-200 bg-orange-600 flex items-center rounded-md text-white' >
+            <BiShoppingBag className='w-[1.3rem] h-[1.3rem] sm:w-[1.7rem] sm:h-[1.7rem]'/>
+          </button>
+          <HiBars3BottomRight onClick={openNav} className='lg:hidden w-[2rem] h-[2rem] text-black'/>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
